7f2fb00385ab62f82daa9d2a0799169e8a8de99d
Smertig <akaraevz@mail.ru>
2020-10-11 19:21:23 +0300

    Bump version to 0.5.1

--- source/utils/mod_info.hpp
+++ source/utils/mod_info.hpp
@@ -5,7 +5,7 @@
 namespace mod_info {
 
 constexpr std::string_view name                    = "Replay Mod";
-constexpr std::string_view version                 = "0.5.0";
+constexpr std::string_view version                 = "0.5.1";
 constexpr std::string_view compatible_game_version = "2020.9.22";
 
 } // namespace mod_info
